d5f918e743230c74d13312e05901eb9ad7e22356
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 13:02:52 +0100

    edit parse type

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,10 +5,10 @@
 let array = [];
 
 for (let i = 1; i <= 6; i++) {
-  let number = parseInt(prompt(`Inserisci numero ${i} di 6:`));
+  let number = parseFloat(prompt(`Inserisci numero ${i} di 6:`));
 
   if (!isNaN(number)) {
-    number = parseInt(number);
+    number = parseFloat(number);
     if (number % 2 !== 0) {
       array.push(number);
     }
