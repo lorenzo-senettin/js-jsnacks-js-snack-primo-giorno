377c52f62cb36535127595f351bf507161dd7d73
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 11:14:59 +0100

    add first word longer

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,3 +4,7 @@
 const first_word = prompt("Inserisci la prima parola:")  // ask first word
 const second_word = prompt("Inserisci la seconda parola:")  // ask second word
 
+if (first_word.length > second_word.length) {
+  console.log(first_word);
+  console.log(second_word);
+} else if (second_word.length > first_word.length)
