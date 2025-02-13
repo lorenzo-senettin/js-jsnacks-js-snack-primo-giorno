cab9d595b64a5034dd987bdeadda80cb9953aaa4
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 10:30:16 +0100

    add number prompt

---
+++ snack-1/script.js
@@ -0,0 +1,6 @@
+// l'utente inserisce due numeri in successione, con due prompt
+// il software stampa il maggiore
+
+const first_number = parseInt(prompt("Inserisci il primo numero")); // ask the user for the first number
+
+const second_number = parseInt(prompt("Inserisci il secondo numero")); // ask the user for the second number 
