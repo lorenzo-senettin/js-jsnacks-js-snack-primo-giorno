5835d189511e5ebd219f09299ecc97724193e965
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 10:34:16 +0100

    add solution

--- snack-1/script.js
+++ snack-1/script.js
@@ -4,3 +4,11 @@
 const first_number = parseInt(prompt("Inserisci il primo numero")); // ask the user for the first number
 
 const second_number = parseInt(prompt("Inserisci il secondo numero")); // ask the user for the second number 
+
+if (first_number > second_number) {
+  console.log(first_number);
+} else if (second_number == first_number) {
+  console.log("I numeri inseriti sono uguali");
+} else {
+  console.log(second_number);
+}
