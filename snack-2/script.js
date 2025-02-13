2617835234993905fec5e9ad70a1b6dea305fde2
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 11:12:42 +0100

    add prompt

---
+++ snack-2/script.js
@@ -0,0 +1,6 @@
+// l'utente inserisce due parole in successione, con due prompt
+// il software stampa prima la parola più corta, poi la più lunga
+
+const first_word = prompt("Inserisci la prima parola:")  // ask first word
+const second_word = prompt("Inserisci la seconda parola:")  // ask second word
+
