f000867b2f72a1202c64a8efd4ae22df61d40171
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 12:46:02 +0100

    add prompt and push if odd

--- snack-4/script.js
+++ snack-4/script.js
@@ -2,3 +2,14 @@
 // chiedi per 6 volte all'utente di inserire un numero
 // se è dispari inseriscilo nell'array
 
+let array = [];
+
+for (let i = 1; i <= 6; i++) {
+  let numero = parseInt(prompt(`Inserisci numero ${i} di 6:`));
+
+  if (numero % 2 !== 0) {
+    array.push(numero);
+  }
+}
+
+console.log(array);
